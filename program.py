d25b29ad8b4767cc053cf0578c948cc97e20359d
kevinsuttits21 <93242927+kevinsuttits21@users.noreply.github.com>
2021-12-04 19:26:46 +0200

    Added new features to EasyGui

--- program.py
+++ program.py
@@ -1,8 +1,84 @@
 from pytube import YouTube, Playlist
 import os
+from easygui import *
 
-choose = input("Do you want import one song (\"song\") or playlist (\"playlist\")? ".lower())
-if choose == "song":
+
+
+text = "Do you want to import one video \"video\" or playlist \"playlist\"? "
+title = "Youtube Video Downloader"
+input_list = ["Answer"]   
+output2 = multenterbox(text,title,input_list)
+
+k = 0
+if output2[0] == "video":
+    k = k + 5
+else:
+    k = k + 0
+    
+if output2[0] == "playlist":
+    k = k + 5
+else:
+    k = k + 0
+    
+
+
+if k < 1:
+    title = "Youtube Video Downloader"
+    message000 = ("Wrong information! Please relaunch the program and insert the correct details.")
+    msgbox(message000, title)
+    output2.close()
+elif k <= 1:
+    title = "Youtube Video Downloader"
+    message000 = ("Wrong information! Please relaunch the program and insert the correct details.")
+    msgbox(message000, title)
+    output2.close()
+    
+    
+
+
+kast2 = msgbox
+text2 = "Enter the following details"
+title2 = "Youtube Video Downloader"  
+input_list2 = ["URL", "Video type (mp3, MP3 or mp4, MP4)"]   
+output = multenterbox(text2, title2, input_list2)
+
+
+if "http" not in output[0]:
+    title = "Youtube Video Downloader"
+    message000 = ("Wrong information! Please relaunch the program and insert the correct details.")
+    msgbox(message000, title)
+    output.close()
+    
+j = 0  
+if output[1] == "MP4" or output[1] == "mp4":
+    j = j + 5
+else:
+    j = j + 0
+    
+if output[1] == "MP3" or output[1] == "mp3":
+    j = j + 5
+else:
+    j = j + 0
+    
+if j < 1:
+    title = "Youtube Video Downloader"
+    message000 = ("Wrong information! Please relaunch the program and insert the correct details.")
+    msgbox(message000, title)
+    output2.close()
+elif j <= 1:
+    title = "Youtube Video Downloader"
+    message000 = ("Wrong information! Please relaunch the program and insert the correct details.")
+    msgbox(message000, title)
+    output2.close()
+    
+
+title3 = "Youtube Video Downloader"  
+message3 = ("Thank you!" + " Please wait for the video to download :)")
+kast = msgbox(message3, title3)
+
+
+choose = input("Do you want to import one video (\"video\") or playlist (\"playlist\")? ".lower())
+if choose == "video":
     url = str(input("Enter the link of YouTube video you want to download:  "))
     yt = YouTube(url)
 elif choose == "playlist":
@@ -10,12 +86,12 @@ elif choose == "playlist":
     yt = Playlist(url)
 
 valik = input("Do you want MP3 or MP4? ")
-if valik == "MP3":
+if valik == "MP3" or "mp3":
     nr = 251
 else:
     nr = 137
     
-if choose == "song":
+if choose == "video":
     print("----------------------------------------")
     print("Title: ",yt.title)
     print("Views: ",yt.views)
@@ -23,7 +99,7 @@ if choose == "song":
     print("Rating of video: ", round(yt.rating, 1))
     print("----------------------------------------")
     print(f'Downloading: {yt.title}')
-    if valik == "MP3":
+    if valik == "MP3" or "mp3":
         vide = yt.streams.get_by_itag(nr)
         downloaded_file = vide.download()
         base, ext = os.path.splitext(downloaded_file)
@@ -40,7 +116,7 @@ elif choose == "playlist":
     print("----------------------------------------")
     print(f'Downloading: {yt.title}')
     for video in yt.videos:
-        if valik == "MP3":
+        if valik == "MP3" or "mp3":
             video = video.streams.get_by_itag(nr)
             downloaded_file = video.download()
             base, ext = os.path.splitext(downloaded_file)
