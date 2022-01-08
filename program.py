4b42777412d286bbec7654efcf08d236afa9c7c0
Kermo Nurmeoja <25592156+kermonurmeoja@users.noreply.github.com>
2022-01-08 19:01:47 +0200

    Added file checking and error codes

--- program.py
+++ program.py
@@ -1,45 +1,24 @@
-from pytube import YouTube, Playlist, Search
-import os
-from easygui import *
+from pytube import YouTube, Playlist
 import getpass
-from os import path
-from os.path import exists
-import sys
 import os.path
+from pathlib import Path
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
-
-
-# my_video = yt.streams.first() my_video.download(r"C:\Users\{user}\Downloads")
-# C:\Users\{user}\Downloads
-askings = input("Please enter your download location: ")
-if askings == "":
-    askings = askings
-    print("The download location will be the directory where you are using the code")
+location = input("Please enter your download location: ")
+if location == "":
+    location = location
+    print("The download location will be the directory where you are using the code.")
 else:
-    askings = 'C:/Users/' + getpass.getuser() + '/' + str(askings)
-    bruh = askings
-    print("Checking if directory is valid")
-    path = str(bruh)
+    location = 'C:/Users/' + getpass.getuser() + '/' + str(location)
+    print("Checking if directory is valid...")
+    path = str(location)
     if_file_exists = os.path.exists(path)
-    print(if_file_exists)
+    print(str(if_file_exists))
 
-              
 choose = input("Do you want to import one video (\"video\") or playlist (\"playlist\")? ").lower()
-
-
 if choose == "video":
-    
-    search_or_video = str(input("Search or enter link? "))
-    if search_or_video == "search":
-        search = Search(input("Search: "))
-        print(str(len(search.results)) + ": ")
-        tulemus = str(search.results)
-        url = tulemus[0]
-    elif search_or_video == "link":
-        url = str(input("Enter the link of YouTube video you want to download:  "))
-    else:
-        exit()
-        
+    url = str(input("Enter the link of YouTube video you want to download:  ")) ## kui pole url, siis ...
     yt = YouTube(url)
     try:
         asd = yt.title
@@ -48,44 +27,58 @@ if choose == "video":
         print(str(viga))
         exit()
 elif choose == "playlist":
-    url = str(input("Enter the link of YouTube playlist you want to download:  "))
+    url = str(input("Enter the link of YouTube playlist you want to download:  ")) ## kui pole url, siis ...
     yt = Playlist(url)
-
-valik = input("Do you want MP3 or MP4? ").lower()
-if valik =="mp3":
-    nr = 251
-elif valik == "mp4":
-    nr = 137
 else:
+    print("Error code 102")
     exit()
 
+extension = input("Do you want MP3 or MP4? ").lower()
+if extension == "mp3":
+    nr = 251
+    if Path(location + "/" + yt.title + ".mp3").is_file():
+        print("File exist. Skipping.")
+        exit()
+    else:
+        print("File not exist")
+elif extension == "mp4":
+    nr = 137
+    if Path(location + "/" + yt.title + ".mp4").is_file():
+        print("File exist. Skipping.")
+        exit()
+    else:
+        print("File not exist")
+else:
+    print("Error code 103")
+    exit()
 
 if choose == "video":
     print("----------------------------------------")
-    print("Title: ",yt.title)
-    print("Views: ",yt.views)
-    print("Length of video: ",yt.length) ## teisendada
+    print("Title: ", yt.title)
+    print("Views: ", yt.views)
+    print("Length of video: ", yt.length)  ## teisendada
     print("Rating of video: ", (yt.rating, 1))
     print("----------------------------------------")
     print(f'Downloading: {yt.title}')
-    if valik == "mp3":
+    if extension == "mp3":
         vide = yt.streams.get_by_itag(nr)
-        downloaded_file = vide.download(str(askings))
+        downloaded_file = vide.download(str(location))
         base, ext = os.path.splitext(downloaded_file)
         new_file = base + '.mp3'
         os.rename(downloaded_file, new_file)
         print("Download completed")
-    elif valik == "mp4":
+    elif extension == "mp4":
         vide = yt.streams.get_highest_resolution()
-        vide.download(str(askings))
+        vide.download(str(location))
         print("Download completed")
     else:
+        print("Error code 104")
         exit()
 elif choose == "playlist":
     print("----------------------------------------")
     print("Playlist name: ", yt.title)
     print("Views: ", {yt.views})
-    print("Length of playlist: ",yt.length) ## teisendada
+    print("Length of playlist: ", yt.length)  ## teisendada
     print('Number of videos in playlist: %s' % len(yt.video_urls))
     print("----------------------------------------")
     print(f'Downloading: {yt.title}')
@@ -95,21 +88,25 @@ elif choose == "playlist":
         except Exception:
             print(f"Video {video} is unavailable, skipping.")
         else:
-            if valik == "mp3":
+            if extension == "mp3":
                 video = video.streams.get_by_itag(nr)
-                downloaded_file = video.download(str(askings))
-                base, ext = os.path.splitext(downloaded_file)
-                new_file = base + '.mp3'
-                os.rename(downloaded_file, new_file)
-                print("Download completed")
-            elif valik == "mp4":
+                if Path(location + "/" + video.title + ".mp3").is_file() == True:
+                    print(f'Video {video.title} is exists, skipping.')
+                else:
+                    downloaded_file = video.download(str(location))
+                    base, ext = os.path.splitext(downloaded_file)
+                    new_file = base + '.mp3'
+                    os.rename(downloaded_file, new_file)
+                    print("Download " + str(video.title) + " completed")
+            elif extension == "mp4":
                 vide = video.streams.get_highest_resolution()
-                vide.download(str(askings))
-                print("Download completed")
+                if Path(location + "/" + vide.title + ".mp3").is_file() == True:
+                    print(f'Video {vide.title} is exists, skipping.')
+                else:
+                    vide.download(str(location))
+                    print("Download completed")
             else:
+                print("Error code 105")
                 exit()
 
-print("Checking if the file is successfully downloaded")
-
-                
-
+print("The process is completed.")
